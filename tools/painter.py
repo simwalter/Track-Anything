7eebcf5c78667d44891be86ef789cd576ae44f00
simwalter <74860600+simwalter@users.noreply.github.com>
2023-07-20 11:50:02 +0200

    Update painter.py

--- tools/painter.py
+++ tools/painter.py
@@ -104,9 +104,14 @@ color_list = color_list.astype('uint8').tolist()
 
 
 def vis_add_mask(image, mask, color):
+	pixel_image = image.copy()
+	height, width = pixel_image.shape[:2]
+	w, h = (16, 16)
+	temp = cv2.resize(input, (w, h), interpolation=cv2.INTER_LINEAR)
+	pixel_image = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
 	color = np.array(color_list[color])
 	mask = mask > 0.5
-	image[mask] = color
+	image[mask] = pixel_image
 	return image.astype('uint8')
 
 def point_painter(input_image, input_points, point_color=5, point_alpha=0.9, point_radius=15):
