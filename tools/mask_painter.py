b18500d6f6c6d4966f6bd50aef9a763f69a748a0
simwalter <74860600+simwalter@users.noreply.github.com>
2023-07-20 02:52:42 +0200

    Update mask_painter.py

--- tools/mask_painter.py
+++ tools/mask_painter.py
@@ -180,7 +180,7 @@ def mask_generator_11(mask, background_radius, contour_radius):
 	return background_mask, contour_mask
 
 
-def mask_painter(input_image, input_mask, background_alpha=0.5, background_blur_radius=7, contour_width=3, contour_color=0, contour_alpha=1, mode='11'):
+def mask_painter(input_image, input_mask, background_alpha=0, background_blur_radius=7, contour_width=3, contour_color=0, contour_alpha=1, mode='11'):
 	"""
 	Input:
 	input_image: numpy array
