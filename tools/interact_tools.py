56ed6ea6a3a64d212c767637845d66a29c8d8e64
simwalter <74860600+simwalter@users.noreply.github.com>
2023-07-20 14:12:05 +0200

    Update interact_tools.py

--- tools/interact_tools.py
+++ tools/interact_tools.py
@@ -7,7 +7,7 @@ from typing import Union
 from segment_anything import sam_model_registry, SamPredictor, SamAutomaticMaskGenerator
 import matplotlib.pyplot as plt
 import PIL
-from .mask_painter import mask_painter as mask_painter2
+# from .mask_painter import mask_painter as mask_painter2
 from .base_segmenter import BaseSegmenter
 from .painter import mask_painter, point_painter
 import os
